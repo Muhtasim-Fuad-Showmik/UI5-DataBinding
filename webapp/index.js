896b8b739964bcd380619f4617961ebeb9559fd5
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-09-11 18:05:15 +0600

    Implemented Two Way Binding on the application

--- webapp/index.js
+++ webapp/index.js
@@ -1,20 +1,25 @@
 sap.ui.require([
-    "sap/m/Text",
-    "sap/ui/model/json/JSONModel"
-], function(Text, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/mvc/XMLView"
+], function(JSONModel, XMLView) {
     "use strict";
 
     // Attach an anonymous function to the SAPUI5 'init' event
 	sap.ui.getCore().attachInit(function () {
         // Create a JSON model from an object literal.
         var oModel = new JSONModel({
-            greetingText: "Hi, my name is Muhtasim Fuad"
+            firstName: "Muhtasim",
+            lastName: "Fuad",
+            enabled: true,
+            panelHeaderText: "Data Binding Basics"
         });
 
         // Assign the model object ot the SAPUI5 Core
         sap.ui.getCore().setModel(oModel);
 
-		// Create a text UI element that displays a hardcoded text string
-		new Text({text: "Hi, my name is Muhtasim Fuad"}).placeAt("content");
+        //Display the XML view called "App"
+        new XMLView({
+            viewName: "sap.ui.demo.db.view.App"
+        }).placeAt("content");
 	});
 });
